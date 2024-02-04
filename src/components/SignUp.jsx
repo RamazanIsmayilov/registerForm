2d5d3a4b69b2f1d9ef4b9ab7a455636a51e777fe
Ramazan <ismayiloff522@gmail.com>
2024-02-04 18:12:20 +0400

    first commit

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,18 @@
+import Logo from '../img/logo.png'
+
+export const SignUp = () => {
+  return (
+    <div className="register rounded d-flex flex-wrap">
+        <div className="left rounded p-3">
+            <div className="heading d-flex align-items-center gap-3">
+                <img width={90} src={Logo} alt="" />
+                <span>Register Form</span>
+            </div>
+        </div>
+        <div className="right">
+            <span>asdfgbasdfghbnjm</span>
+        </div>
+    </div>
+  )
+}
+
